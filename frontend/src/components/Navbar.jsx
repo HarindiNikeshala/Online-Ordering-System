f7221b800e205806f8fcdd812bd3727927bc5832
harindinikeshala <harindinikeshala@gmail.com>
2025-02-25 14:26:41 +0530

    Adding Navbar commit

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,47 @@
+import { Container, Flex, HStack, Text, Button, useColorMode } from "@chakra-ui/react"
+import { CiSquarePlus } from "react-icons/ci";
+import { Link } from "react-router-dom";
+import { IoMdMoon, IoMdSunny } from "react-icons/io";
+
+const Navbar = () => {
+
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  return (
+    <Container maxW={"1140px"} px={4}>
+      <Flex
+        h={16}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        flexDir={{
+          base: "column",
+          sm: "row"
+        }}
+      >
+        <Text
+          fontSize={{ base: "22", sm: "28" }}
+          bgGradient='linear(to-l, #7928CA, #FF0080)'
+          bgClip='text'
+          textTransform={"uppercase"}
+          textAlign={"center"}
+          fontWeight='extrabold'
+        >
+          <Link to={"/"}>Product Store</Link>
+        </Text>
+
+        <HStack spacing={2} alignItems={"center"}>
+          <Link to={"/create"}>
+            <Button>
+              <CiSquarePlus fontSize={20} />
+            </Button>
+          </Link>
+          <Button onClick={toggleColorMode}>
+            {colorMode === "light" ? <IoMdMoon size="20" /> : <IoMdSunny size="20" />}
+          </Button>
+        </HStack>
+      </Flex>
+    </Container>
+  );
+};
+
+export default Navbar;
