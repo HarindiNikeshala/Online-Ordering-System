516367fcf949b78ddf033b6c31cae458ac76f538
harindinikeshala <harindinikeshala@gmail.com>
2025-02-25 12:09:34 +0530

    Updating backend part commit

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import productRouters from "./routes/product.route.js"
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // allows us to accept JSON data in the req.body
 
@@ -14,7 +15,7 @@ app.use("/api/products", productRouters);
 
 // console.log(process.env.MONGO_URL);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     connectDB();
-    console.log("Server started at http://localhost:5000");
+    console.log("Server started at http://localhost:" + PORT);
 });
