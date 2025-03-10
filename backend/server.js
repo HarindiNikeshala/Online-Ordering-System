3102f6da0153d74ec0bceb2af72482421cccc6e8
harindinikeshala <harindinikeshala@gmail.com>
2025-03-10 12:52:23 +0530

    both get same port

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 import { connectDB } from './config/db.js';
-import productRouters from "./routes/product.route.js"
+import productRoutes from "./routes/product.route.js"
 
 
 dotenv.config();
@@ -9,11 +10,21 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json()); // allows us to accept JSON data in the req.body
 
-app.use("/api/products", productRouters);
+app.use("/api/products", productRoutes);
 
 // console.log(process.env.MONGO_URL);
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+    });
+
+}
 
 app.listen(PORT, () => {
     connectDB();
